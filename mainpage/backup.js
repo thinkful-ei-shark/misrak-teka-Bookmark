a94bc88a76e81ff64e678f57e16ea329ac23d0ca
Teka <bk2188mb@go.minnstate.edu>
2020-10-05 01:52:06 -0500

    2

--- mainpage/backup.js
+++ mainpage/backup.js
@@ -1,10 +1,4 @@
 "use strict";
-// this is where the AJAX logic is
-// function getWeburl(weburl) {
-//   fetch("https://faceebook.com")
-//     .then((response) => response.jason())
-//     .then((responseJason) => console.log(responseJason));
-// }
 
 function getbookmark(url) {
   fetch("https://thinkful-list-api.herokuapp.com/misrak/bookmarks")
@@ -22,15 +16,7 @@ function render() {
 
   console.log(store);
 }
-// watch for clinte submitting the form
-// function watchForm() {
-//   $("form").submit((event) => {
-//     event.preventDefault();
-//     getWeburl();
-//   });
-// }
-//fetch render div elment title rating discrpiit hide
-//click
+
 $(function () {
   console.log("loading pls wait!");
   getbookmark();
